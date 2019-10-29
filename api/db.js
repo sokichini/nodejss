f7054c551d6ccff916bb345ec8ef2b7f49090649
Vũ Quang Hòa <36296959+sokichini@users.noreply.github.com>
2019-10-29 09:46:48 +0700

    edit

--- api/db.js
+++ api/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "vtgoroot",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "nodejs_api"
 });
 
